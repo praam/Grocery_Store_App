b7526498cdb07aab5ffa37aa7b0fa6104044b939
praam <connnectopramod@gmail.com>
2020-11-24 08:02:07 -0800

    Backend code for products

--- Backend/server.py
+++ Backend/server.py
@@ -1,18 +1,40 @@
-from flask import  Flask , request , jsonify
+from flask import Flask, request, jsonify
+from sql_connection import get_sql_connection
+import mysql.connector
+import json
+
 import products_dao
-from sql_connections import get_sql_connection
 
 app = Flask(__name__)
 
 connection = get_sql_connection()
 
-@app.route('/get_products',methods=['GET'])
+@app.route('/getProducts', methods=['GET'])
 def get_products():
-    products = products_dao.get_all_products(connection)
-    response = jsonify(products)
-    response.headers.add('Access-Control-Allow-Origin','*')
+    response = products_dao.get_all_products(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-if __name__== "__main__":
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+    request_payload = json.loads(request.form['data'])
+    product_id = products_dao.insert_new_product(connection, request_payload)
+    response = jsonify({
+        'product_id': product_id
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/deleteProduct', methods=['POST'])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form['product_id'])
+    response = jsonify({
+        'product_id': return_id
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == "__main__":
     print("Starting Python Flask Server For Grocery Store Management System")
     app.run(port=5000)
